13cbca227d9eeb55d1ef31e41c5b8839e2d31858
Phearong <phearong349@gmail.com>
2023-07-28 15:18:16 +0700

    feat: config head meta tag

---
+++ config/head.ts
@@ -0,0 +1,14 @@
+export const head = {
+    titleTemplate: '%s - client',
+    title: 'client',
+    htmlAttrs: {
+        lang: 'en'
+    },
+    meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
+import * as config from './config'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   ssr: false,
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - client',
-    title: 'client',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
@@ -67,5 +50,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  ...config,
 }
